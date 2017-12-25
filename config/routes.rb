824a55312281bdc173dcaf600633824144c26a93
briancclin <briancclin@gmail.com>
2017-12-24 22:48:55 -0800

    Change root page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  root 'questions#index'
 end
