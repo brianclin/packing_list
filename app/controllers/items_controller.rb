977a38fcf7c5dfc7014410e16373786871176ab9
briancclin <briancclin@gmail.com>
2017-12-24 23:46:53 -0800

    Added restart to navigate back to beginning

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -78,6 +78,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :category_id, :event_id, :weather_id, :transporation_id, :international, :redeye)
+      params.require(:item).permit(:name, :category_id, :event_id, :weather_id, :transporation_id, :international, :redeye, :domestic)
     end
 end
