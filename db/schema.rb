3fe3a5eefae35191e500120ed0f0d1cde9a10772
Brian <briancclin@gmail.com>
2019-06-29 23:29:40 -0700

    Update gems

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 20171226054130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "adminpack"
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
