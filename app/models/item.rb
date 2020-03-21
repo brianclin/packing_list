2704401344ce6272fc410fbf5e61a4220aed47a9
Brian <briancclin@gmail.com>
2020-03-21 15:58:51 -0700

    Rubocop item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,35 +1,33 @@
+# frozen_string_literal: true
+
 class Item < ApplicationRecord
   validates :event_id, presence: false
 
-
   def clothing?
     clothing_list = [
-        'pants',
-        'shorts',
-        'swimsuit',
-        'sweater',
-        'heels',
-        'boots',
-        'outfit',
-        'jeans',
-        'gloves',
-        'scarf',
-        'dress',
-        'tank tops',
-        'shirts',
-        'socks',
-        'jacket',
-        'hat',
-        'sandals',
-        'flip flops',
-        'shoes'
+      'pants',
+      'shorts',
+      'swimsuit',
+      'sweater',
+      'heels',
+      'boots',
+      'outfit',
+      'jeans',
+      'gloves',
+      'scarf',
+      'dress',
+      'tank tops',
+      'shirts',
+      'socks',
+      'jacket',
+      'hat',
+      'sandals',
+      'flip flops',
+      'shoes'
     ]
     clothing_list.each do |clothing_item|
-      if name.downcase.include? clothing_item
-        return true
-      end
+      return true if name.downcase.include? clothing_item
     end
-    return false
+    false
   end
 end
-
