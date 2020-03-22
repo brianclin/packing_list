bc72e2c05cfd215c0d2fcb14b2b1d330911a7aa5
Brian <briancclin@gmail.com>
2020-03-21 22:01:28 -0700

    Check if clothing from clothing.txt

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,27 +4,8 @@ class Item < ApplicationRecord
   validates :event_id, presence: false
 
   def clothing?
-    clothing_list = [
-      'pants',
-      'shorts',
-      'swimsuit',
-      'sweater',
-      'heels',
-      'boots',
-      'outfit',
-      'jeans',
-      'gloves',
-      'scarf',
-      'dress',
-      'tank tops',
-      'shirts',
-      'socks',
-      'jacket',
-      'hat',
-      'sandals',
-      'flip flops',
-      'shoes'
-    ]
+    clothing_list = File.readlines('db/data/clothing.txt')
+    clothing_list = clothing_list.map(&:strip)
     clothing_list.each do |clothing_item|
       return true if name.downcase.include? clothing_item
     end
