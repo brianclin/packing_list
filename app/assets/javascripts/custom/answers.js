d93475b9a2cd6253a9194a680cb8d4dbcd1acd18
Brian <briancclin@gmail.com>
2020-03-29 17:44:24 -0700

    Add eslint

--- app/assets/javascripts/custom/answers.js
+++ app/assets/javascripts/custom/answers.js
@@ -1,8 +1,8 @@
-function deleteItem(button) {
-    inline = button.parentNode
-    formCheck = inline.parentNode
-    col = formCheck.parentNode
-    col.removeChild(formCheck)
-    item = formCheck.getElementsByClassName("form-check-label")[0].textContent
-    $.post( "/answers/remove", { item: item} )
+function deleteItem (button) { // eslint-disable-line no-unused-vars
+  var inline = button.parentNode
+  var formCheck = inline.parentNode
+  var col = formCheck.parentNode
+  col.removeChild(formCheck)
+  var item = formCheck.getElementsByClassName('form-check-label')[0].textContent
+  $.post('/answers/remove', { item: item }) /* eslint-env jquery */
 }
