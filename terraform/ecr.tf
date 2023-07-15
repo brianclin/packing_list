401b10847a242bc6a49d97aa2107a10bc8931c2d
Brian Lin <briancclin@gmail.com>
2023-07-14 23:50:06 -0400

    ci: create ecr repo

---
+++ terraform/ecr.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "packing_list" {
+  name                 = "packing_list"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
